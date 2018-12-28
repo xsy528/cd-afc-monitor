89bf67b56ad4a33dd979c56c35862eda19ddef2a
徐哲民 <xuzhemin@unittec.com>
2018-12-28 10:48:11 +0800

    1.删除未使用的新项目 2.删除Lib项目

--- prev/3-project/workbench/workbench-monitor/src/main/java/com/insigma/acc/workbench/monitor/dao/MetroDeviceDao.java
+++
@@ -1,7 +0,0 @@
-package com.insigma.acc.workbench.monitor.dao;
-
-import com.insigma.acc.workbench.monitor.model.entity.MetroDevice;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MetroDeviceDao extends JpaRepository<MetroDevice,Short> {
-}

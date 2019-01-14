44b59111364984e8e89774af0b401a8ecaef2d42
xuzhemin <xuzhemin@unittec.com>
2019-01-14 10:57:36 +0800

    1.增加错误码

--- prev/3-project/WZACCWorkbench/src/main/java/com/insigma/acc/wz/web/exception/ErrorCode.java
+++ prev/3-project/WZACCWorkbench/src/main/java/com/insigma/acc/wz/web/exception/ErrorCode.java
@@ -19,7 +19,13 @@ public enum ErrorCode {
     READ_PARAMETER_ERROR(8,"读取请求参数异常"),
     THRESHOLD_INVALID(9,"报警阀值不得低于警告阀值，参数设置失败"),
     REFRESH_INTERVAL_INVALID(10,"刷新周期不能小于5秒"),
+    SAVE_FILE_FAILED(11,"保存文件到本地失败"),
+    NODE_EXISTS(12,"节点已经存在"),
+    NODE_NOT_EXISTS(13,"节点不存在"),
+    LINEID_EMPTY(14,"线路编号不能为空"),
+    INVALID_ARGUMENT(15,"非法参数"),
     UNKNOW_ERROR(100,"未知异常"),
+    TOO_LARGE_FILE(101,"文件过大"),
     MONITOR_CONFIG_SAVE_ERROR(1000,"监控参数设置失败");
 
 

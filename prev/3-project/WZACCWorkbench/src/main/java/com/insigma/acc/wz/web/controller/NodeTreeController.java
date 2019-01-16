cae09789e80658f969ccd87affc44a2f172f8061
xuzhemin <xuzhemin@unittec.com>
2019-01-16 15:35:31 +0800

    1.去掉获取图片列表的接口 2.修改各个方法名称 3.修改获取设备分组树的接口路径

--- prev/3-project/WZACCWorkbench/src/main/java/com/insigma/acc/wz/web/controller/NodeTreeController.java
+++ prev/3-project/WZACCWorkbench/src/main/java/com/insigma/acc/wz/web/controller/NodeTreeController.java
@@ -3,7 +3,6 @@ package com.insigma.acc.wz.web.controller;
 import com.fasterxml.jackson.annotation.JsonView;
 import com.insigma.acc.wz.web.model.vo.NodeItem;
 import com.insigma.acc.wz.web.model.vo.Result;
-import com.insigma.acc.wz.web.service.FileService;
 import com.insigma.acc.wz.web.service.NodeService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -12,44 +11,35 @@ public class NodeTreeController extends BaseMultiActionController {
     @Autowired
     private NodeService nodeService;
 
-    @Autowired
-    private FileService fileService;
-
     static {
-        methodMapping.put("/node/list/editor","editorTreeList");
-        methodMapping.put("/node/list/monitor","monitorTreeList");
-        methodMapping.put("/node/list/simple","simpleTreeList");
-        methodMapping.put("/node/list/simpleDeviceGroup","simpleDeviceGroupTreeList");
-        methodMapping.put("/node/list-img-urls","imageList");
+        methodMapping.put("/node/list/editor","editorTree");
+        methodMapping.put("/node/list/monitor","monitorTree");
+        methodMapping.put("/node/list/simple","simpleTree");
+        methodMapping.put("/node/list/device","deviceTree");
     }
 
-    public NodeTreeController(NodeService nodeService, FileService fileService){
+    public NodeTreeController(NodeService nodeService){
         this.nodeService = nodeService;
-        this.fileService = fileService;
     }
 
     @JsonView(NodeItem.editor.class)
-    public Result<NodeItem> editorTreeList(){
+    public Result<NodeItem> editorTree(){
         return nodeService.getNodeTree();
     }
 
     @JsonView(NodeItem.monitor.class)
-    public Result<NodeItem> monitorTreeList(){
+    public Result<NodeItem> monitorTree(){
         return nodeService.getNodeTree();
     }
 
     @JsonView(NodeItem.required.class)
-    public Result<NodeItem> simpleTreeList(){
+    public Result<NodeItem> simpleTree(){
         return nodeService.getNodeTree();
     }
 
     @JsonView(NodeItem.required.class)
-    public Result<NodeItem> simpleDeviceGroupTreeList(){
+    public Result<NodeItem> deviceTree(){
         return nodeService.getNodeSimpleTree();
-    }
-
-    public Result<String[]> imageList(){
-        return fileService.getResourceList();
     }
 
 }

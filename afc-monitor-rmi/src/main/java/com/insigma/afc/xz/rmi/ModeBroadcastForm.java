15c9e7d53e1d45b323620bdcbf662f36638fa19d
xuzhemin <xuzhemin@unittec.com>
2019-03-15 18:48:08 +0800

    增加rmi接口数据模型

---
+++ afc-monitor-rmi/src/main/java/com/insigma/afc/xz/rmi/ModeBroadcastForm.java
@@ -0,0 +1,58 @@
+/* 
+ * 日期：2019年3月11日
+ *  
+ * 版权所有：浙江浙大网新众合轨道交通工程有限公司
+ */
+/**
+ * 
+ */
+package com.insigma.afc.xz.rmi;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Ticket:  车站模式广播请求
+ * @author  matianming
+ *
+ */
+public class ModeBroadcastForm implements Serializable {
+
+	private static final long serialVersionUID = -6533498242844877344L;
+
+	private List<ModeStation> modeStationList = new ArrayList<>();
+
+	public List<ModeStation> getModeStationList() {
+		return modeStationList;
+	}
+
+	public void setModeStationList(List<ModeStation> modeStationList) {
+		this.modeStationList = modeStationList;
+	}
+
+	public static class ModeStation {
+
+		private Integer stationId;
+
+		private Integer modeCode;
+
+		public Integer getStationId() {
+			return stationId;
+		}
+
+		public void setStationId(Integer stationId) {
+			this.stationId = stationId;
+		}
+
+		public Integer getModeCode() {
+			return modeCode;
+		}
+
+		public void setModeCode(Integer modeCode) {
+			this.modeCode = modeCode;
+		}
+
+	}
+
+}

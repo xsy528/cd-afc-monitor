5b4f2f18fdc60b3d07cf0f026d28c84ea1cd2283
xingshaoya <1234578>
2019-03-20 11:36:31 +0800

    Merge remote-tracking branch 'remotes/origin/master' into XSY
    # Conflicts:
    #	afc-monitor-service/src/main/java/com/insigma/afc/monitor/service/impl/ModeServiceImpl.java
    去除无用注释，修改设备事件查询逻辑

--- afc-monitor-service/src/main/java/com/insigma/afc/monitor/service/impl/DeviceEventServiceImpl.java
+++ afc-monitor-service/src/main/java/com/insigma/afc/monitor/service/impl/DeviceEventServiceImpl.java
@@ -1,9 +1,6 @@
 package com.insigma.afc.monitor.service.impl;
 
-import com.insigma.afc.monitor.constant.OrderDirection;
 import com.insigma.afc.monitor.dao.TmoEquEventDao;
-import com.insigma.afc.monitor.dao.TmoEquStatusCurDao;
-import com.insigma.afc.monitor.dao.TmoItemStatusDao;
 import com.insigma.afc.monitor.model.dto.condition.DeviceEventCondition;
 import com.insigma.afc.monitor.model.entity.TmoEquStatus;
 import com.insigma.afc.monitor.service.DeviceEventService;
@@ -18,7 +15,7 @@ import java.util.Date;
 import java.util.List;
 
 /**
- * 设备事件查询接口
+ * Tiket:设备事件查询接口
  * @author  xingshaoya
  */
 @Service
@@ -28,26 +25,20 @@ public class DeviceEventServiceImpl implements DeviceEventService {
     private TmoEquEventDao tmoEquEventDao;
     /**
      * 获取设备事件
-     * @param condition
-     * @return
+     * @param condition 设备事件查询实体类
+     * @return 分页数据
      */
     @Override
     public Page<TmoEquStatus> getDeviceEventSearch(DeviceEventCondition condition) {
         //根据总线路、分支线路、站点并在开始时间和结束时间内，并且事件等级（非用户限制）in(1,2,3)内
         //节点id
-        List<Long> nodeIds = condition.getNodeIds();
+        Long[] nodeIds = condition.getNodeIds();
         //开始时间
         Date startTime = condition.getStartTime();
         //结束时间
         Date endTime = condition.getEndTime();
-        //设备类型数组
-        List<Short> devTypeList = condition.getDevTypeList();
-        //最大条数
-        Integer maxCount = condition.getMaxCount();
-        //排序字段
-        String orderField = condition.getOrderField();
-        //排序方向
-        OrderDirection orderType = condition.getOrderType();
+        //事件等级
+        Short eventLevel = condition.getEventLevel();
 
         Integer pageSize = condition.getPageSize();
         Integer page = condition.getPageNumber();
@@ -56,19 +47,23 @@ public class DeviceEventServiceImpl implements DeviceEventService {
         return tmoEquEventDao.findAll((root,query,builder)->{
             List<Predicate> predicates = new ArrayList<>();
             if (nodeIds!=null){
-                predicates.add(root.get("nodeId").in(nodeIds));  //将站点ID存入root中
+                predicates.add(root.get("nodeId").in(nodeIds));
+                //将站点ID存入root中
+            }
+            if (eventLevel!=null){
+                predicates.add(builder.equal(root.get("nodeId"),eventLevel));
+                //将站点ID存入root中
             }
             if (startTime!=null){
-                predicates.add(builder.greaterThanOrEqualTo(root.get("occurTime"),startTime));//获取开始时间之后
+                predicates.add(builder.greaterThanOrEqualTo(root.get("occurTime"),startTime));
+                //获取开始时间之后
             }
             if (endTime!=null){
-                predicates.add(builder.lessThanOrEqualTo(root.get("occurTime"),endTime));//获取结束时间之前
+                predicates.add(builder.lessThanOrEqualTo(root.get("occurTime"),endTime));
+                //获取结束时间之前
             }
-            //事件等级
-//            if (nodeIds!=null){
-//                predicates.add(root.get("nodeId").in(nodeIds));  //将站点ID存入root中
-//            }
-            query.orderBy(builder.desc(root.get("occurTime")));//以modeUploadtime降序
+            query.orderBy(builder.desc(root.get("occurTime")));
+            //以occurTime降序
             return builder.and(predicates.toArray(new Predicate[0]));
         }, PageRequest.of(page,pageSize));
     }

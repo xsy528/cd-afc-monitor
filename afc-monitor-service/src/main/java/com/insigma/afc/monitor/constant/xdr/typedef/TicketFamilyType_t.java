84f1ff3d2ef0cca30ba92b295f78e8a12711b329
xuzhemin <xuzhemin@unittec.com>
2019-04-12 11:19:45 +0800

    修改xdr

--- afc-monitor-service/src/main/java/com/insigma/afc/monitor/constant/xdr/typedef/TicketFamilyType_t.java
+++ afc-monitor-service/src/main/java/com/insigma/afc/monitor/constant/xdr/typedef/TicketFamilyType_t.java
@@ -1,27 +1,32 @@
 package com.insigma.afc.monitor.constant.xdr.typedef;
 
-/*
- *;
- */
 public interface TicketFamilyType_t {
-	// 单程票 ;
-	public final static int TF_SJT = 0x01;
-	// 出站票 ;
-	public final static int TF_EXIT = 0x02;
-	// 计程票 ;
-	public final static int TF_MILE = 0x03;
-	// 计时票 ;
-	public final static int TF_TIME = 0x04;
-	// 计次票 ;
-	public final static int TF_RIDE = 0x05;
-	// 员工票 ;
-	public final static int TF_STAFF = 0x06;
-	// 市民卡 ;
-	public final static int TF_CITIZEN = 0x07;
-	// 手机卡 ;
-	public final static int TF_MOBILE = 0x08;
-	//银联卡;
-	public final static int TF_UNIONPAY = 0x09;
-	//二维码;
-	public final static int TF_QRCODE = 0x0A;
+	public final static int TFT_TFTSavingCard = 1;
+	public final static int TFT_TFTCashCard = 2;
+	public final static int TFT_TFTStudentCard = 3;
+	public final static int TFT_TFTTheOldCard = 4;
+	public final static int TFT_TFTConventionCard = 5;
+	public final static int TFT_TFTThirdPartyCard = 6;
+	public final static int TFT_TFTInterconnectCard = 7;
+	public final static int TFT_TFTTalentGreenCard = 8;
+	public final static int TFT_TFTCommonCard = 9;
+	public final static int TFT_TFTFinanceICCard = 20;
+	public final static int TFT_TFTQrTicket = 21;
+	public final static int TFT_TFTQrTicketForMetro = 22;
+	public final static int TFT_CountSouvenirTicket = 50;
+	public final static int TFT_RegularSouvenirTicket = 51;
+	public final static int TFT_FixedSouvenirTicket = 52;
+	public final static int TFT_FixedSouvenirTicket1 = 53;
+	public final static int TFT_RechargeTimeLimitTicket = 54;
+	public final static int TFT_FixedSouvenirTicket2 = 55;
+	public final static int TFT_DailyTicket = 60;
+	public final static int TFT_ServiceValueTicket = 84;
+	public final static int TFT_ServiceCard = 85;
+	public final static int TFT_WorkCard = 86;
+	public final static int TFT_PoliceCard = 87;
+	public final static int TFT_StaffCard = 88;
+	public final static int TFT_SpecialStaffCard = 89;
+	public final static int TFT_ZeroTicket = 97;
+	public final static int TFT_SJTTicket = 98;
+	public final static int TFT_SJTSouvenirTicket = 99;
 }
